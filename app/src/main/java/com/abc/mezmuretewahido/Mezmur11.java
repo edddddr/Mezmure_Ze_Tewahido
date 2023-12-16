01dfbdca1d41fd3d2780dab6ffe6196c20695a1a
edddddr <endt6342@gmail.com>
2023-12-15 23:17:22 -0800

    The sub class of Mawicha4 and That dispaly on The layout

---
+++ app/src/main/java/com/abc/mezmuretewahido/Mezmur11.java
@@ -0,0 +1,68 @@
+package com.abc.mezmuretewahido;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class Mezmur11 extends AppCompatActivity {
+    ArrayAdapter mAdapter;
+    TextView mEmptyView;
+    Toolbar mToolbar;
+    ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mezmur11);
+
+        mToolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        String[] tStory=getResources().getStringArray(R.array.title_story11);
+        String[] dStory=getResources().getStringArray(R.array.detail_story11);
+
+        listView =(ListView) findViewById(R.id.list2);
+        mEmptyView = (TextView) findViewById(R.id.emptyVeiw);
+
+
+
+        listView=findViewById(R.id.list2);
+
+
+        mAdapter = new ArrayAdapter(Mezmur11.this,
+                android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.title_story11));
+        listView.setAdapter(mAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String t=dStory[position];
+                Intent intent=new Intent(Mezmur11.this,zirzir.class);
+                intent.putExtra("story",t);
+                startActivity(intent);
+            }
+        });
+        listView.setEmptyView(mEmptyView);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId()==android.R.id.home){
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
