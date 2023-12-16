cddc3fd724fcc1f107dcc9614253bc41a25c6012
edddddr <endt6342@gmail.com>
2023-12-15 22:44:49 -0800

    The Thired Alternative CardView that functional getting into the box

---
+++ app/src/main/java/com/abc/mezmuretewahido/Mawicha3.java
@@ -0,0 +1,66 @@
+package com.abc.mezmuretewahido;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+public class Mawicha3 extends AppCompatActivity implements View.OnClickListener {
+    private CardView Meze1;
+    CardView Meze2;
+    CardView Meze3;
+    Toolbar mToolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mawicha3);
+
+        mToolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        Meze1 = (CardView) findViewById(R.id.Meze1);
+        Meze2 = (CardView) findViewById(R.id.Meze2);
+        Meze3 = (CardView) findViewById(R.id.Meze3);
+
+
+
+        Meze1.setOnClickListener(this);
+        Meze2.setOnClickListener(this);
+        Meze3.setOnClickListener(this);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId()==android.R.id.home){
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+        switch (v.getId()) {
+            case R.id.Meze1:
+                i = new Intent(this,Mezmur7.class);startActivity(i);break;
+
+            case R.id.Meze2:
+                i = new Intent(this,Mezmur8.class);startActivity(i);break;
+
+            case R.id.Meze3:
+                i = new Intent(this,Mezmur9.class);startActivity(i);break;
+            default:break;
+        }
+
+    }
+}
